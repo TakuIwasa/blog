8c512c709c1fa7d6d969ce8ab9ae501e457f6e37
TakuIwasa <dolphinscry0713@gmail.com>
2021-03-20 10:09:00 +0900

    fix production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 import environ
 
-ALLOWED_HOSTS = ['hitokotomono-db.cfadru8nyjdr.ap-northeast-1.rds.amazonaws.com']
+ALLOWED_HOSTS = ['LoadBalancerWeb-580883579.ap-northeast-1.elb.amazonaws.com']
 
 # settings.pyの位置を起点として３つ上の親ディレクトリを参照。
 BASE_DIR = environ.Path(__file__) - 3
